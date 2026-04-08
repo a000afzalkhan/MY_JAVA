29b019b799b92245262e6f7539dedc5968502e30
a000afzalkhan <000heyafzal@gmail.com>
2026-04-08 08:31:28 +0530

    Solved Minimize_max_1877

--- src/LeetCode/Minimize_max_1877.java
+++ src/LeetCode/Minimize_max_1877.java
@@ -3,6 +3,24 @@ package LeetCode;
 import java.util.Arrays;
 
 public class Minimize_max_1877 {
-//    public static int minMaxPair(int [] nums){
-//    }
+    public static int minMaxPair(int [] nums){              // { 1,2,3,4,5 }
+        Arrays.sort(nums);
+
+        int left =0;
+        int right = nums.length-1;
+        int max = 0;
+
+        while(left<right){
+            int sum =nums[left] + nums[right];
+            max = Math.max(max ,sum);
+            left ++;
+            right --;
+        }
+        return max;
+
+    }
+
+    static void main(String[] args) {
+
+    }
 }
--- src/Test_Coding/Reverse_integer.java
+++ src/Test_Coding/Reverse_integer.java
@@ -25,3 +25,18 @@ public class Reverse_integer {
         }
     }
 }
+/*
+class Solution {
+    public int reverse(int x) {
+        int rev=0;
+        while(x!=0){
+            if (rev > Integer.MAX_VALUE / 10 || rev < Integer.MIN_VALUE / 10) {
+                return 0;
+            }
+            rev=rev*10+x%10;
+            x/=10;
+        }
+        return rev;
+    }
+}
+ */
