aa4d5a4825e46fafc97407acd5ca3a5dcf8ab95f
a000afzalkhan <000heyafzal@gmail.com>
2026-04-13 12:18:17 +0530

    solve

---
+++ src/LeetCode/Find_third_maxNumber_414.java
@@ -0,0 +1,69 @@
+package LeetCode;
+
+import java.util.TreeSet;                                   // One more time to understand
+
+public class Find_third_maxNumber_414 {
+    static void main(String[] args) {
+
+    }
+
+    public int thirdMax(int[] nums) {
+        TreeSet<Integer> set = new TreeSet<>();
+
+        for (int num : nums) {
+            set.add(num);
+            if (set.size() > 3) {
+                set.pollFirst();
+            }
+        }
+
+        return (set.size() == 3) ? set.first() : set.last();
+    }
+}
+
+/*
+
+class Solution {
+    public int thirdMax(int[] nums) {
+        // Three variables to store maxiumum three numbers till now.
+        long firstMax = Long.MIN_VALUE;
+        long secondMax = Long.MIN_VALUE;
+        long thirdMax = Long.MIN_VALUE;
+
+        for (int num : nums) {
+            // This number is already used once, thus we skip it.
+            if (firstMax == num || secondMax == num || thirdMax == num) {
+                continue;
+            }
+
+            // If current number is greater than first maximum,
+            // It means that this is the greatest number and first maximum and second max
+            // will become the next two greater numbers.
+            if (firstMax <= num) {
+                thirdMax = secondMax;
+                secondMax = firstMax;
+                firstMax = num;
+            }
+            // When current number is greater than second maximum,
+            // it means that this is the second greatest number.
+            else if (secondMax <= num) {
+                thirdMax = secondMax;
+                secondMax = num;
+            }
+            // It is the third greatest number.
+            else if (thirdMax <= num) {
+                thirdMax = num;
+            }
+        }
+
+        // If third max was never updated, it means we don't have 3 distinct numbers.
+        if (thirdMax == Long.MIN_VALUE) {
+            int ans = (int) firstMax;
+            return ans;
+        }
+
+        int ans = (int) thirdMax;
+        return ans;
+    }
+}
+ */
---
+++ src/LeetCode/Sqrt_69.java
@@ -0,0 +1,12 @@
+package LeetCode;
+
+public class Sqrt_69 {
+    static void main(String[] args) {
+
+        System.out.println(numberSqrt(8));
+
+    }
+    public static double numberSqrt (int num){
+            return (int) Math.sqrt(num) ;                   // (int) is casting hai
+    }
+}
