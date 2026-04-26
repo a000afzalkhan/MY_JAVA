f042ec02e2ace082cf52d5f585bf73632c94b781
a000afzalkhan <000heyafzal@gmail.com>
2026-04-26 23:10:11 +0530

    3834

---
+++ src/LeetCode/myRough/M_3834.java
@@ -0,0 +1,27 @@
+package LeetCode.myRough;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class M_3834 {
+    static void main(String[] args) {
+    }
+}
+
+
+    /*
+    public List<Long> mergeAdjacent(int[] nums){
+
+        List<Long> wapas = new ArrayList<>();
+
+        for (int i : nums) {
+            long val = i;
+
+            while (!wapas.isEmpty() && wapas.get(wapas.size() - 1 ) == val){
+
+            }
+
+        }
+        return wapas;
+}
+*/
