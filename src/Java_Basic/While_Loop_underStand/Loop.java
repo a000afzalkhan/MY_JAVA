72028bbd4e32ad587df722df0d35ce147a8d88c9
a000afzalkhan <000heyafzal@gmail.com>
2026-05-02 22:21:53 +0530

    loop understand

--- src/Java_Basic/While_Loop_underStand/Loop.java
+++ src/Java_Basic/While_Loop_underStand/Loop.java
@@ -3,20 +3,39 @@ package Java_Basic.While_Loop_underStand;
 public class Loop {
     static void main(String[] args) {
 
-        for (int i = 0; i <5 ; i++) {
-            System.out.println(i);
+        int[] nums = {4, 5, 1, 2};
+        int tar = 6;
+
+        boolean found = false;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+
+                if (nums[i] + nums[j] == tar) {
+                    System.out.println(i + ", " + j);
+                    found = true;
+                    break;
+                }
+            }
+            if (found) break;
         }
 
-        int i = 0;
-        while (i<5){
-            System.out.print(i);
-            i++;
+        if (!found) {
+            System.out.println("-1, -1");
         }
-
-
-
     }
 }
+
+//        for (int i = 0; i <5 ; i++) {
+//            System.out.println(i);
+//        }
+//
+//        int i = 0;
+//        while (i<5){
+//            System.out.print(i);
+//            i++;
+//        }
+
 
 /*
                             IN WHILE LOOP
