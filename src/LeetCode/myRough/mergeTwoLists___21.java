bc45ec131ac9f8c8aec5f2c53a0f9686f28ef9e9
a000afzalkhan <000heyafzal@gmail.com>
2026-05-14 23:41:50 +0530

    21 Rough

--- src/LeetCode/myRough/mergeTwoLists___21.java
+++ src/LeetCode/myRough/mergeTwoLists___21.java
@@ -1,4 +1,16 @@
 package LeetCode.myRough;
 
 public class mergeTwoLists___21 {
+    class linkedList{
+        int val;
+        linkedList next;
+
+        //Constructor
+        linkedList(){}
+        linkedList(int val){ this.val=val; }
+        linkedList(int val,linkedList next){  this.val=val;  this.next=next; }
+
+    }
+    static void main(String[] args) {}
+
 }
